c25375c48ea3683e2228a3922e56d6a1f29a5136
Yuka Langbuana <langbuana.yuka@hotmail.com>
2019-01-02 15:21:31 -0800

    beautify things

--- js/feed.js
+++ js/feed.js
@@ -20,6 +20,34 @@ function getJSON(url) {
   return resp ;
 }
 
+function timeSince(date) {
+
+  var seconds = Math.floor((new Date() - date) / 1000);
+
+  var interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) {
+    return interval + "y";
+  }
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) {
+    return interval + "m";
+  }
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) {
+    return interval + "d";
+  }
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) {
+    return interval + "h";
+  }
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) {
+    return interval + "m";
+  }
+  return Math.floor(seconds) + "s";
+}
+
 var gjson = JSON.parse(getJSON(API_URL));
 var content =gjson.results
 
@@ -29,12 +57,11 @@ for (i = content.length-1; i > 0; i--) {
         <div class="row">
             <div class="col-sm-2"><button type="submit" class="box-btn"><span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span></button></div>
             <div class="col-sm-10">
-                <p>${content[i].content}</p>
-                <div class="name"><p>${content[i].name}</p></div>
+                <p class="content">${content[i].content}</p>
+                <div class="name"><p>${content[i].name}, ${timeSince(new Date(content[i].time))} ago</p></div>
             </div>
         </div>
     </div>
 `;
   document.getElementById("feeds").innerHTML += html
-  console.log(content[i].content)
 }
