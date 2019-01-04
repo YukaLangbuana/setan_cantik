34f01792b85563b5c882317954aae9b4ff8bdf92
Yuka Langbuana <langbuana.yuka@hotmail.com>
2019-01-04 05:42:01 -0800

    style upgrade

--- js/feed.js
+++ js/feed.js
@@ -1,25 +1,3 @@
-const form = document.querySelector('form')
-const API_URL = "https://kuntilanak.herokuapp.com/api/v1/setan/"
-const postElement = document.querySelector('.feeds')
-
-
-function getJSON(url) {
-  var resp ;
-  var xmlHttp ;
-
-  resp  = '' ;
-  xmlHttp = new XMLHttpRequest();
-
-  if(xmlHttp != null)
-  {
-      xmlHttp.open( "GET", url, false );
-      xmlHttp.send( null );
-      resp = xmlHttp.responseText;
-  }
-
-  return resp ;
-}
-
 function timeSince(date) {
 
   var seconds = Math.floor((new Date() - date) / 1000);
@@ -46,22 +24,4 @@ function timeSince(date) {
     return interval + "m";
   }
   return Math.floor(seconds) + "s";
-}
-
-var gjson = JSON.parse(getJSON(API_URL));
-var content =gjson.results
-
-for (i = content.length-1; i > 0; i--) { 
-  var html = `
-  <div class="body">
-        <div class="row">
-            <div class="col-sm-2"><button type="submit" class="box-btn"><span class="glyphicon glyphicon-heart-empty" aria-hidden="true"></span></button></div>
-            <div class="col-sm-10">
-                <p class="content">${content[i].content}</p>
-                <div class="name"><p>${content[i].name}, ${timeSince(new Date(content[i].time))} ago</p></div>
-            </div>
-        </div>
-    </div>
-`;
-  document.getElementById("feeds").innerHTML += html
 }
--- js/main.js
+++
@@ -1,5 +0,0 @@
-$(function($) {
-    $(document).on('click', '.box-btn', function(event) {
-      $(this).find('.glyphicon').toggleClass('red').toggleClass('glyphicon-heart-empty glyphicon-heart');
-    });
-  });
